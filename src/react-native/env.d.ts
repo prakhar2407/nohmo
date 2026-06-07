302987089758086666766ab5206b376d669d413f
Prakhar <prakhar.bansal@webest.dev>
2026-06-08 00:02:45 +0530

    Fix getReferrer() method

--- src/react-native/env.d.ts
+++ src/react-native/env.d.ts
@@ -35,7 +35,8 @@ declare module 'react-native' {
     getInitialURL: () => Promise<string | null>
   }
 
-  export const NativeModules: Record<string, Record<string, unknown>>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  export const NativeModules: Record<string, any>
 }
 
 declare module '@react-native-async-storage/async-storage' {
